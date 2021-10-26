2923082f3ec7e630be08ba09c8008425c4cf061d
RT <me@rtflores.com>
2021-10-26 16:27:43 -0700

    change App.js to a functional component

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,11 @@ import './App.css';
 import Quote from './Quote';
 import Title from './Title';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <Title />
-        <Quote />
-        <p>{this.props.messages[0]}</p>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="container">
+      <Title />
+      <Quote />
+    </div>
+  );
 }
