c4540628d47860c95d268325bd53233c5533ed15
RT <me@rtflores.com>
2021-10-28 15:11:30 -0700

    add terminal.css through npm

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './store';
 import './index.css';
+import 'terminal.css';
 
 ReactDOM.render(
   <Provider store={store}>
