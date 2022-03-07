2dc7cfc8f452242a8b880e6862710323f48aae49
RT Flores <me@rtflores.com>
2022-03-06 17:30:11 -0800

    centralize theme

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './store';
 import './index.css';
-import 'terminal.css';
 
 ReactDOM.render(
   <Provider store={store}>
